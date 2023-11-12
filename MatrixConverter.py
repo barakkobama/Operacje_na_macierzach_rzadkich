008ec8fa69669fc1d7f0934d052ced0d467137da
barakkobama <jczykier@wp.pl>
2023-11-12 12:34:10 +0100

    added function for generating matrixes

--- MatrixConverter.py
+++ MatrixConverter.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import csr_matrix, csc_matrix
 
 def matrix_to_csr(matrix):
     values = []
@@ -60,3 +61,20 @@ def csc_to_matrix(values, column_indices, row_pointers):
 
     return matrix
 
+def generate_sparse_matrix(dimensions, zero_percentage, format='list'):
+    rows, cols = dimensions
+    total_elements = rows * cols
+    num_zeros = int((zero_percentage / 100) * total_elements)
+    num_nonzeros = total_elements - num_zeros
+
+    elements = np.array([0]*num_zeros + [np.random.randint(1, 10) for _ in range(num_nonzeros)])
+    np.random.shuffle(elements)
+
+    matrix = elements.reshape(rows, cols)
+
+    if format == 'csr':
+        return csr_matrix(matrix)
+    elif format == 'csc':
+        return csc_matrix(matrix)
+    else:
+        return matrix.tolist()
--- example.py
+++ example.py
@@ -1,14 +1,9 @@
 from MatrixConverter import *
-from LU import LU_CPU, LU_GPU
+# from LU import LU_CPU, LU_GPU
 
 
 # Example usage
-example_matrix = [
-    [10, 0, 0, 0],
-    [0, 20, 0, 0],
-    [0, 0, 30, 0],
-    [0, 0, 0, 40]
-]
+example_matrix = generate_sparse_matrix((10,10), 80, 'list')
 
 # Convert to CSR and back
 csr_values, csr_row_indices, csr_column_pointers = matrix_to_csr(example_matrix)
@@ -18,11 +13,13 @@ restored_matrix_from_csr = csr_to_matrix(csr_values, csr_row_indices, csr_column
 csc_values, csc_column_indices, csc_row_pointers = matrix_to_csc(example_matrix)
 restored_matrix_from_csc = csc_to_matrix(csc_values, csc_column_indices, csc_row_pointers)
 
-print("Original Matrix:", example_matrix)
+print("Original Matrix:")
+for row in example_matrix:
+    print(row)
 print(f"CSR \nV: {csr_values} \nCOL_INDEX: {csr_row_indices} \nROW_INDEX: {csr_column_pointers}")
 print("Restored from CSR:\n", restored_matrix_from_csr)
 print(f"CSC \nV: {csc_values} \nCOL_INDEX: {csc_column_indices} \nROW_INDEX: {csc_row_pointers}")
 print("Restored from CSC:\n", restored_matrix_from_csc)
 
-print(LU_CPU(example_matrix))
-print(LU_GPU(example_matrix))
+# print(LU_CPU(example_matrix))
+# print(LU_GPU(example_matrix))
